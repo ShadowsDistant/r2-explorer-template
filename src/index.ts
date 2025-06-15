f90447044568f0d6e486128a543c9128eb8dcf76
DistantShadows <shadowsdistant@gmail.com>
2025-06-15 01:34:25 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,4 @@
-import { R2Explorer } from "r2-explorer";
 
-export default R2Explorer({
-  // Set to false to allow users to upload files
-  readonly: false,
 
   import { R2Explorer } from 'r2-explorer';
 
@@ -10,9 +6,4 @@ export default R2Explorer({ readonly: false, basicAuth: {
     username: 'anotherUser',
     password: '123567'
   }
-});
-
-  // Learn more how to secure your R2 Explorer instance:
-  // https://r2explorer.com/getting-started/security/
-  // cfAccessTeamName: "my-team-name",
 });
